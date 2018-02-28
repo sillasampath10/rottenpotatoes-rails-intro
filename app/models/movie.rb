52767a2d61f8b2d0166243aa7f940140ac92d634
sillasampath10 <sillasampath10@gmail.com>
2018-02-28 09:33:06 +0000

    Part 3 trials

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
-
+   def self.all_ratings
+   	 result = {}
+   	 self.select(:rating).uniq.each do |movie|
+   		result[movie.rating] = 1
+   	 end
+   	 return result
+   end
 end
