bcbf38fb25ba203a00eaa7f285efe6cd2bb481be
sillasampath10 <sillasampath10@gmail.com>
2018-02-28 09:29:30 +0000

    Part 3 trials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,17 +11,28 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked_ratings = check
-    @checked_ratings.each do |rating|
-      params[rating] = true
+    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
+      if(session.has_key?(:sort) || session.has_key?(:ratings))
+        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+      end
     end
-    
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
+    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
+    @all_ratings = Movie.all_ratings.keys
+    @ratings = params[:ratings]
+    if(@ratings != nil)
+      ratings = @ratings.keys
+      session[:ratings] = @ratings
     else
-      @movies = Movie.where(:rating => @checked_ratings)
+      if(!params.has_key?(:commit) && !params.has_key?(:sort))
+        ratings = Movie.all_ratings.keys
+        session[:ratings] = Movie.all_ratings
+      else
+        ratings = session[:ratings].keys
+      end
     end
+    @checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
+    @movies = Movie.order(@sort).where(rating: @checked_ratings)
+    @mark = ratings
   end
 
   def new
@@ -52,6 +63,17 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def ratings
+    ratings = Array.new
+    movies = Movie.all
+    movies.each do |movie|
+       ratings.push movie[:rating]
+       ratings.uniq!
+    end
+    return ratings
+  end
+  
+  
   private
 
   def check
@@ -61,4 +83,9 @@ class MoviesController < ApplicationController
       @all_ratings
     end
   end
+  def find_class(header)
+    params[:sort] == header ? 'hilite' : nil
+  end
+  helper_method :find_class
+
 end
